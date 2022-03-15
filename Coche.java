12e37d28631590a7ec5d23a86e9584e99acf744a
Daviiid999 <david.luque@itponiente.com>
2022-03-15 13:06:23 +0100

    Ultimas modificaciones

--- Coche.java
+++ Coche.java
@@ -18,28 +18,28 @@ class Coche{
     public Motor getMotor(){
       return this.motorCoche;
     }
-    public void abrirVentanaDelD(){
+    public void abrirVentanaD(){
       this.puertaDelD.getVentana().abrir();
     }
-    public void abrirVentanaDelI(){
-      this.puertaDelD.getVentana().abrir();
-    }
+
 
     //Funcion para abrir la puerta que quiera
     public void abrirPuertas(int numero){
       if (numero == 1) {
-        System.out.println(miCoche.getPuertaDelI().abrir() + "La puerta delantera izquierda se ha abierto");
-      } else if (numero == 2) {
-        System.out.println(miCoche.getPuertaDelD().abrir() + "La puerta delantera derecha se ha abierto");
+        this.puertaDelI.abrir();
+      }
+      else if (numero == 2) {
+        this.puertaDelD.abrir();
       }
     }
 
     //Funcion para abrir la ventana que quiera
     public void abrirVentanas(int numero){
       if (numero == 1) {
-        System.out.println(miCoche.getPuertaDelI().getVentanaDelI().abrir() + "La ventana delantera izquierda se ha abierto");
-      } else if (numero == 2) {
-        System.out.println(miCoche.getPuertaDelD().getVentanaDelD().abrir() + "La ventana delantera derecha se ha abierto");
+        this.puertaDelI.getVentana().abrir();
+      }
+      else if (numero == 2) {
+        this.puertaDelD.getVentana().abrir() ;
       }
     }
 
